93d30d834cd39d525fd1b8e7011b8368af24bc5b
root <xingjian2066@gmail.com>
2023-08-31 23:39:18 +0800

    change readme

--- generate.py
+++ generate.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     entrypoint_main()
 
 
-# python generate.py --inference_server="openai_azure_chat:AIGA_MODEL_0::" --base_model=gpt-3.5-turbo-16k --h2ocolors=False --langchain_mode=UserData
